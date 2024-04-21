6ad2f59c00d53cc67cf0fe399333f96cead737ba
Piyush289kumar <piyushraikwar289@gmail.com>
2024-04-21 17:26:15 +0530

    Create Components

---
+++ client/src/component/Create.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Create() {
+  return (
+    <div>
+      <input type="text" name="createTodo" id="createTodo" />
+      <button type="submit">Add</button>
+    </div>
+  );
+}
+
+export default Create;
