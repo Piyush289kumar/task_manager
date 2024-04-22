5536252563b81da1f3f3071db497f42ec2af8fdf
Piyush289kumar <piyushraikwar289@gmail.com>
2024-04-22 11:12:49 +0530

    Updated

--- server/index.js
+++ server/index.js
@@ -13,18 +13,30 @@ mongoose.connect(
   "mongodb+srv://piyushraikwar289:piyush289@cluster0.5lpcsrr.mongodb.net/"
 );
 
+app.get("/", (_, res) => {
+  Todo.find()
+    .then((result) => res.status(200).json(result))
+    .catch((err) => res.status(500).json(err.message));
+});
 app.post("/add", (req, res) => {
   Todo.create({ task: req.body.task })
     .then((result) => res.status(200).json(result))
     .catch((err) => res.status(500).json(err.message));
 });
 
-app.get("/get", (_, res) => {
-  Todo.find()
+app.put("/update/:id", (req, res) => {
+  const { id } = req.params;
+  Todo.findByIdAndUpdate({ _id: id }, { isTaskDone: true })
     .then((result) => res.status(200).json(result))
-    .catch((err) => res.status(500).json(err.message));
+    .catch((err) => res.status(401).json(err));
 });
 
+app.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
+  Todo.findByIdAndDelete({ _id: id })
+    .then((result) => res.status(200).json(result))
+    .catch((err) => res.status(401).json(err));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
