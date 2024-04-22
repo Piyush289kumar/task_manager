2f8ae8cf8a0bfd4fa3ddc995e6c36a125642076b
Piyush289kumar <piyushraikwar289@gmail.com>
2024-04-22 11:12:28 +0530

    Build Todo App In MERN

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,17 +1,30 @@
 import React, { useState, useEffect } from "react";
 import { Create } from "./index.js";
 import axios from "axios";
-import { FaTrash, FaCircleNotch } from "react-icons/fa";
+import { FaTrash, FaCircleNotch, FaCheckCircle } from "react-icons/fa";
 
 function Home() {
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
     axios
-      .get("http://127.0.0.1/3001/get")
-      .then((res) => setTodos(res))
+      .get("http://127.0.0.1:3001/")
+      .then((res) => setTodos(res.data))
       .catch((err) => console.log(err));
   }, []);
+
+  const doneHandler = (id) => {
+    axios
+      .put("http://127.0.0.1:3001/update/" + id)
+      .then(() => location.reload)
+      .catch((err) => console.log(err.message));
+  };
+  const deleteHandler = (id) => {
+    axios
+      .delete("http://127.0.0.1:3001/delete/" + id)
+      .then(() => location.reload)
+      .catch((err) => console.log(err.message));
+  };
 
   return (
     <div>
@@ -23,9 +36,12 @@ function Home() {
       ) : (
         todos.map((todo, idx) => (
           <div key={idx}>
-            <FaCircleNotch />
+            <div onClick={() => doneHandler(todo._id)}>
+              {todo.isTaskDone ? <FaCheckCircle /> : <FaCircleNotch />}
+            </div>
             <p>{todo.task}</p>
-            <FaTrash />
+
+            <FaTrash onClick={() => deleteHandler(todo._id)} />
           </div>
         ))
       )}
